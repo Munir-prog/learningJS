551a0aa4be992c0c13668d3145288585e5c5aee9
Munir Zikrullaev <munirprog@gmail.com>
2021-10-06 19:06:30 +0300

    learn about sets in js

---
+++ set.js
@@ -0,0 +1,24 @@
+const set = new Set([1, 2, 3, 3, 3, 4, 5, 5, 6])
+
+console.log(set)
+
+set.add(30).add(50).add(80)
+
+console.log(set)
+
+console.log(set.has(50));
+
+console.log(set.size)
+
+console.log(set.delete(2))
+console.log(set.has(2))
+// set.clear()
+// console.log(set.size);
+
+console.log(set.entries())
+
+for (let key of set) {
+    console.log(key)
+}
+
+
